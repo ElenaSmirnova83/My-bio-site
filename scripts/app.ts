8c8d0d4253ab02de5a31468a771d4a5b30aaadba
Elena Smirnova <natanelenasmirnovy@gmail.com>
2022-10-16 13:01:25 -0400

    added some changes

--- scripts/app.ts
+++ scripts/app.ts
@@ -1,17 +1,25 @@
 "use strict";
 //IIFE -- Immediately Invoked Function Expression
 // AKA - Self Executing Function 
-(function () {
+(function()
+{
+
+
     // First method of using functions
-    function Start() {
+    function Start()
+    {
         console.log("App Started!");
+
         // LoadData("GET", "./Data/contacts.json", function(XHR){
         //     console.log(XHR);
         // });
-        $.getJSON("./Data/contacts.json", function (DataSource) {
+
+        $.getJSON("./Data/contacts.json", function(DataSource){
             console.log(DataSource.ContactList[0]);
         });
     }
+
+
     window.addEventListener("load", Start);
+
 })();
-//# sourceMappingURL=app.js.map
